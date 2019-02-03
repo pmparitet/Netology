90720238fab296ead26d6d3a50202bfc74b93e2a
pmparitet <47057210+pmparitet@users.noreply.github.com>
2019-02-03 17:58:49 +0300

    Add files via upload

---
+++ SQL/hw2.sql
@@ -0,0 +1,81 @@
+SELECT 'Морозов Александр Федорович';
+-- 1. Простые выборки
+-- 1.1 SELECT , LIMIT - выбрать 10 записей из таблицы rating (Для всех дальнейших запросов выбирать по 10 записей, если не указано иное)
+SELECT *
+FROM public.ratings
+LIMIT 10;
+
+-- 1.2 WHERE, LIKE - выбрать из таблицы links всё записи, у которых imdbid оканчивается на "42", а поле movieid между 100 и 1000
+(
+SELECT *
+FROM public.links
+WHERE
+    movieid >= '100'
+    AND imdbid like '%42'
+) INTERSECT (
+SELECT *
+FROM public.links
+WHERE
+    movieid < '1000'
+    AND imdbid like '%42'
+)
+LIMIT 10;
+
+-- 2. Сложные выборки: JOIN
+-- 2.1 INNER JOIN выбрать из таблицы links все imdbId, которым ставили рейтинг 5
+SELECT *
+FROM public.links
+JOIN public.ratings
+    ON links.movieid=ratings.movieid
+WHERE ratings.rating =5
+LIMIT 10;
+
+-- 3. Аггрегация данных: базовые статистики
+-- 3.1 COUNT() Посчитать число фильмов без оценок
+SELECT
+    COUNT(links.movieid) as count_movie_ratind_0
+FROM public.links
+LEFT JOIN public.ratings
+    ON links.movieid=ratings.movieid
+WHERE ratings.rating IS NULL
+LIMIT 10;
+
+-- 3.2 GROUP BY, HAVING вывести top-10 пользователей, у который средний рейтинг выше 3.5
+SELECT
+    userId,
+    COUNT(rating) as activity
+FROM public.ratings
+GROUP BY userId
+HAVING AVG(rating) > 3.5
+ORDER BY activity DESC
+LIMIT 10;
+
+-- 4. Иерархические запросы
+-- 4.1 Подзапросы: достать любые 10 imbdId из links у которых средний рейтинг больше 3.5.
+SELECT DISTINCT
+    imdbid
+FROM public.links
+JOIN public.ratings
+    ON links.movieid=ratings.movieid
+WHERE
+    rating > (
+            SELECT AVG(rating)
+            FROM public.ratings
+    )
+    AND rating > 3.5
+LIMIT 10;
+
+--4.2 Common Table Expressions: посчитать средний рейтинг по пользователям, у которых более 10 оценок. Нужно подсчитать средний рейтинг по все пользователям, которые попали под условие - то есть в ответе должно быть одно число.
+WITH tmp_user10
+AS (
+    SELECT
+    userid,
+    COUNT(rating) AS r_10
+    FROM public.ratings
+    GROUP BY userid
+)
+SELECT
+AVG(r_10)
+FROM tmp_user10
+WHERE r_10 > 10
+LIMIT 10;
