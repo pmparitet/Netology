46af47b80e2ce17503229378c0452ec1a524df65
pmparitet <47057210+pmparitet@users.noreply.github.com>
2019-02-09 15:37:10 +0300

    Update hw2.sql

--- SQL/hw2.sql
+++ SQL/hw2.sql
@@ -6,24 +6,18 @@ FROM public.ratings
 LIMIT 10;
 
 -- 1.2 WHERE, LIKE - выбрать из таблицы links всё записи, у которых imdbid оканчивается на "42", а поле movieid между 100 и 1000
-(
 SELECT *
 FROM public.links
 WHERE
     movieid >= '100'
+    AND movieid < '1000'
     AND imdbid like '%42'
-) INTERSECT (
-SELECT *
-FROM public.links
-WHERE
-    movieid < '1000'
-    AND imdbid like '%42'
-)
 LIMIT 10;
 
 -- 2. Сложные выборки: JOIN
 -- 2.1 INNER JOIN выбрать из таблицы links все imdbId, которым ставили рейтинг 5
-SELECT *
+SELECT
+    imdbid
 FROM public.links
 JOIN public.ratings
     ON links.movieid=ratings.movieid
@@ -52,30 +46,29 @@ LIMIT 10;
 
 -- 4. Иерархические запросы
 -- 4.1 Подзапросы: достать любые 10 imbdId из links у которых средний рейтинг больше 3.5.
-SELECT DISTINCT
+SELECT
     imdbid
 FROM public.links
-JOIN public.ratings
-    ON links.movieid=ratings.movieid
-WHERE
-    rating > (
-            SELECT AVG(rating)
-            FROM public.ratings
+WHERE movieId IN
+    (
+    SELECT movieId
+    FROM public.ratings
+    GROUP BY movieId
+    HAVING AVG(rating) > 3.5
     )
-    AND rating > 3.5
 LIMIT 10;
 
 --4.2 Common Table Expressions: посчитать средний рейтинг по пользователям, у которых более 10 оценок. Нужно подсчитать средний рейтинг по все пользователям, которые попали под условие - то есть в ответе должно быть одно число.
 WITH tmp_user10
 AS (
     SELECT
-    userid,
-    COUNT(rating) AS r_10
+    userid
     FROM public.ratings
     GROUP BY userid
-)
+    HAVING COUNT(rating) > 10
+    )
 SELECT
-AVG(r_10)
-FROM tmp_user10
-WHERE r_10 > 10
+AVG(rating)
+FROM public.ratings
+WHERE ratings.userid IN (select userid from tmp_user10)
 LIMIT 10;
